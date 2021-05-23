ad59e43325769be9899caace158614b585e31a6e
Guilherme Augusto de Souza <oguilherme@tuta.io>
2021-05-23 10:24:19 -0300

    full basic functionalitys, todo: cryptography

--- nforget.cpp
+++ nforget.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 void store_pass(std::string);
+void read_pass();
 
 int main() {
     std::string password;
@@ -17,12 +18,36 @@ int main() {
 	    store_pass(password);
 	    std::cout << "Password saved on file <Pass>\n";
 	}
+	if(option == 2) {
+	    std::cout << "Registered Password:\n";
+	    std::cout << "-------------------\n";
+	    read_pass();
+	    std::cout << "-------------------\n";
+	}
     } while(option != 3);
 }
 
 void store_pass(std::string password) {
     std::ofstream file;
     file.open("Pass", std::ios::app);
-    file << password << std::endl;
-    file.close();
+    if(file.is_open()) {
+	file << password << std::endl;
+	file.close();
+    } else {
+	std::cout << "Unable to open file\n";
+    }
+}
+
+void read_pass() {
+    std::ifstream file;
+    std::string line;
+    file.open("Pass");
+    if(file.is_open()) {
+	while(std::getline(file, line)) {
+	    std::cout << line << "\n";
+	}
+	file.close();
+    } else {
+	std::cout << "Unable to open file\n";
+    }
 }
